124469b2b1e3562f35443fb85b0015a1102d6a09
Jesus12ASD <88260985+Jesus12ASD@users.noreply.github.com>
2021-09-02 22:46:24 -0500

    Update Código_Arduino.ino

--- Código_Arduino/Código_Arduino.ino
+++ Código_Arduino/Código_Arduino.ino
@@ -1,43 +1,38 @@
-////////////////////////////////
-// Joystick controlled mouse///
-/// by Shubham Santosh////////
-/////////////////////////////
-
-
-void setup() 
-{
+void setup(){
   Serial.begin(9600);
-  pinMode(9,INPUT);     // SW pin
+  pinMode(9,INPUT);     
   digitalWrite(9,HIGH);
-
 }
-int prev_state=0;   // previous state of switch
-void loop() {
+
+int prev_state=0;
+
+void loop(){
   int z=0,xpos=0,ypos=0;
   int x=analogRead(A0);
   int y=analogRead(A1);
-  int sensitivity=10;    // you can adjust the sensitivity based on your comfort
-  if(x>=550)                     // when moved up 
+  int sensitivity=10;    
+  
+  if(x>=550)                     
   xpos=map(x,550,1023,0,sensitivity); 
-  if(x<=450)                   // when moved down
+  if(x<=450)                   
   xpos=map(x,450,0,0,-sensitivity);   
-  if(y>=550)                    // when moved right
+  if(y>=550)                   
   ypos=map(y,550,1023,0,sensitivity);  
-  if(y<=450)                  // when moved left
+  if(y<=450)                  
   ypos=map(y,450,0,0,-sensitivity); 
+ 
   int curr_state=digitalRead(9);
-  if(curr_state==1 && prev_state==0)   // when SW is pressed 
+  if(curr_state==1 && prev_state==0)   
   z=1;
   else
   z=0;
-  if(xpos!=0 or ypos!=0 or z==1) // prints only when the joystick is moved
-  {
-  Serial.print(xpos);    // print the data and separating by ":"
+  if(xpos!=0 or ypos!=0 or z==1){
+  Serial.print(xpos);   
   Serial.print(":");
   Serial.print(ypos);
   Serial.print(":");
   Serial.println(z);
   }
   prev_state=curr_state;
-  delay(10);         // for normal operation
+  delay(10);         
 }
